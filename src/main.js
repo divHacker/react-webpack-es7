efb599469da2e0740dc76764165d00536121be44
Nati Namvong <nati@flyingcomma.com>
2016-02-28 15:18:56 +0700

    edit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,73 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
+var ItemName = React.createClass({
+  render: function() {
+    return (
+      <h1>
+        {this.props.name}
+      </h1>
+    )
+  }
+})
+
+var ItemTel = React.createClass({
+  render: function() {
+    return (
+      <h2>
+        {this.props.tel}
+      </h2>
+    )
+  }
+})
+
+var Item = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ItemName name={this.props.name}/>
+        <ItemTel tel={this.props.tel}/>
+        <hr/>
+      </div>
+    )
+  }
+})
+
+var ListItem = React.createClass({
+  render: function() {
+    let list = this.props.list
+    let listItem = list.map((item) => {
+      if (item.name === 'nati')
+        return (<Item name={item.name}/>)
+      else
+        return (<Item name={item.name} tel={item.tel}/>)
+    })
+    console.log(listItem)
+    return (
+      <div>
+        {listItem}
+      </div>
+    )
+  }
+})
+
+let list = [
+  {
+    name: 'nati',
+    tel:'123'
+  },
+  {
+    name: 'peter',
+    tel:'456'
+  },
+  {
+    name: 'cat',
+    tel:'789'
+  }
+]
 ReactDOM.render(
-  <h1>Hello, world!!!!</h1>,
+  <div>
+    <ListItem list={list}/>
+  </div>,
   document.getElementById('example')
 )
